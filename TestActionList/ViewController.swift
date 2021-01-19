cfe27d12c250b0fac4a90f66ae2191ec3c670c5b
Sergey Tszyu <tszyu@dbosoft.com.ua>
2021-01-19 13:26:32 +0200

    Initial Commit

---
+++ TestActionList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestActionList
+//
+//  Created by Sergey Tszyu on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
